839ee47b76ab28c7298b9491b5414d2903969161
Diego Marczal <dmarczal@gmail.com>
2015-10-15 12:00:09 -0300

    using environment variables for password

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "educacional.c3sl.ufpr.br",
-  :user_name            => APP_CONFIG[:email][:account],
-  #:user_name            => "carrie.ufpr",
-  #:password             => "carrie123",
-  :password             => APP_CONFIG[:email][:passwd],
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
