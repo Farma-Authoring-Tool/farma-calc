c8eefe852c2ce716b43d6eda2dd2de641d5c82c4
Diego Marczal <dmarczal@gmail.com>
2023-02-17 11:03:52 -0300

    Added shell script to deploy

---
+++ script/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git rebase master
+git push origin production
+
+commands="
+  cd ~/farma-calc/farma &&
+  git pull origin production &&
+  docker-compose exec web bundle install &&
+"
+
+if [ "$1" = "--compile-assets" ]; then
+ commands+="
+   docker-compose exec web bundle exec rake assets:precompile &&
+ "
+fi
+
+commands+=" docker-compose restart web "
+
+ssh deployer@200.134.18.140 -t $commands
+
